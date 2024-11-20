8e0c0e764fbb9b471aec625c87e303cc69f5171f
muhammedajmalkt <muhammedajmalkt09@gmail.com>
2024-11-20 10:04:38 +0530

    Time: 0 ms (100%), Space: 49.3 MB (81.85%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    let x=BigInt(num1)
+        let y=BigInt(num2)
+        return  String(x*y)
+
+};
