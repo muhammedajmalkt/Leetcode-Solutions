dcf6fd7b8cf2ffc76c2a61ac358853e8599d3570
muhammedajmalkt <muhammedajmalkt09@gmail.com>
2025-01-04 10:22:13 +0530

    Time: 194 ms (5.08%), Space: 50 MB (60.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+ 
+ for(let i=0;i<=nums.length;i++){
+    for(let j=0;j<=nums.length;j++){
+   if( i !== j && nums[i]+nums[j] === target){
+       return [ i,j]
+    }
+   
+   }
+
+}
+    return []
+};
+ const nums = [2,7,11,15]
