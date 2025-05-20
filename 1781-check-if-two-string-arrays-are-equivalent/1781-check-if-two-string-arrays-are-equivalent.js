9c6c7eaa5373e69011297e1acf47b6cba7a2a20b
muhammedajmalkt <muhammedajmalkt09@gmail.com>
2025-05-20 11:34:23 +0530

    Time: 3 ms (5.42%), Space: 54.4 MB (14.89%) - LeetHub

---
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+let j=word1.join("")
+let y=word2.join("")
+    console.log(j == y )
+    return j == y
+};
