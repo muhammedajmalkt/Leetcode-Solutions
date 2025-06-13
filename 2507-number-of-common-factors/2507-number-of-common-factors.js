af1a1ce3a0666af86d2cb016f856d3d9b28af280
muhammedajmalkt <muhammedajmalkt09@gmail.com>
2025-06-13 16:01:40 +0530

    Time: 1 ms (34.12%), Space: 53.6 MB (67.23%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.js
+++ 2507-number-of-common-factors/2507-number-of-common-factors.js
@@ -5,7 +5,8 @@
  */
 var commonFactors = function(a, b) {
     count =0
-for(let i =0 ;i<= a ;i++){
+    let min = Math.min(a,b)
+for(let i =0 ;i<= min ;i++){
      if(a % i ==0 && b%i ==0 ){
          count++
      }
