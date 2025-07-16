86acb3a16c9dbcd858992dbdf350cf3e177ed27e
MUHAMMED AJMAL <muhammedajmalkt09@gmail.com>
2025-07-16 09:28:09 +0530

    Time: 33 ms (34.69%), Space: 53.4 MB (94.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-    for(let j = i+1;j<nums.length;j++){
-   let k= (nums[i]+nums[j]) == target
-   if(k){
-       console.log([i,j])
-       return  [i,j]
 
-   }
+for (let i = 0; i < nums.length; i++) {
+  for (let j = i + 1; j < nums.length; j++) {
+    if (nums[i] + nums[j] === target) {
+       return [i, j]
     }
+  }
 }
-}
+
+};
